6ff4ab82b01cfc0299dd3f5f7a155e1c1fd5e0bb
Mark Bucciarelli <mkbucc@gmail.com>
2019-01-26 11:55:48 -0500

    Try -md sha256

--- test_runner.sh
+++ test_runner.sh
@@ -342,7 +342,7 @@ if [ "$CI" = "1" ] ; then
   git commit -m "CI test run" >/dev/null 2>&1
 
   # Decode private deploy SSH key
-  openssl aes-256-cbc -k "$travis_key_password" -md md5 -d -a -in travis_key.enc -out ./travis_key
+  openssl aes-256-cbc -k "$travis_key_password" -md sha256 -d -a -in travis_key.enc -out ./travis_key
   echo -e "Host github.com\n  IdentityFile $(pwd)/travis_key" > ~/.ssh/config
   echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" > ~/.ssh/known_hosts
 
