49254c3a0c83dbda75f8c38354a7505b53f398ce
Mark Bucciarelli <mkbucc@gmail.com>
2019-01-26 15:20:07 -0500

    Set perms on private key and use SSH repo URL

--- test_runner.sh
+++ test_runner.sh
@@ -343,8 +343,10 @@ if [ "$CI" = "1" ] ; then
 
   # Decode private deploy SSH key
   openssl aes-256-cbc -k "$travis_key_password" -md sha256 -d -a -in travis_key.enc -out ./travis_key
+  chmod 400 ./travis_key
   echo "Host github.com" > ~/.ssh/config
   echo "  IdentityFile $(pwd)/travis_key" >> ~/.ssh/config
+  git remote set-url origin git@github.com:mbucc/shmig_test.git
   if [ "$DEBUG" ] ; then
     cat ~/.ssh/config
     echo "$(pwd)"
